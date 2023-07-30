ea8e9cdb323e15483847b49024c6ce28fe7189dd
Aamir <aamirasaram2107@gmail.com>
2023-07-30 14:02:25 +0200

    Add build folder for deploying

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { FaSearch, FaChevronLeft } from 'react-icons/fa';
 import './NavBar.css';
+import { useNavigate } from 'react-router-dom';
 
-const NavBar = () => (
-  <nav className="navbar">
-    <FaChevronLeft className="back-icon" />
-    <h2>All Zones</h2>
-    <FaSearch className="search-icon" />
-  </nav>
-);
+const NavBar = () => {
+  const navigate = useNavigate();
+  return (
+    <nav className="navbar">
+      <button
+        type="button"
+        className="back-btn"
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        <FaChevronLeft />
+      </button>
+      <h2>All Zones</h2>
+      <FaSearch className="search-icon" />
+    </nav>
+  );
+};
 
 export default NavBar;
