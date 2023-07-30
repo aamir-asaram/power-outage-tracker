296d64fe8893253818743881b5d8a71d970bf40b
Aamir <aamirasaram2107@gmail.com>
2023-07-30 13:46:01 +0200

    Display schedule in dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import ProvinceList from './components/ProvinceList';
+import Details from './components/Details';
 
 function App() {
   return (
     <>
       <NavBar />
-      <ProvinceList />
+      <Routes>
+        <Route path="/" element={<ProvinceList />} />
+        <Route path="/:province" element={<Details />} />
+      </Routes>
+
     </>
   );
 }
--- src/components/Province.js
+++ src/components/Province.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 const categories = [
   'Buffalo City', 'City Of Cape Town', 'City Power', 'Eastern Cape',
@@ -7,14 +8,8 @@ const categories = [
   'Mpumalanga', 'North West', 'Northern Cape', 'Western Cape',
 ];
 
-const baseURL = 'https://eskom-calendar-api.shuttleapp.rs';
-
 const Province = ({ province }) => {
-  const fetchDetails = async () => {
-    const response = await fetch(`${baseURL}/schedules/${province}`);
-    const data = await response.json();
-    return data;
-  };
+  const navigate = useNavigate();
 
   let str = province.split('-').join(' ');
   let category;
@@ -25,57 +20,8 @@ const Province = ({ province }) => {
     str = str.replace(cat.toLowerCase(), '');
   });
 
-  // function to return the date of the next 3 days
-  const nextThreeDays = () => {
-    const today = new Date();
-    const tomorrow = new Date(today);
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    const dayAfterTomorrow = new Date(today);
-    dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2);
-    const day4 = new Date(today);
-    day4.setDate(day4.getDate() + 3);
-    return [today, tomorrow, dayAfterTomorrow];
-  };
-
-  const findStages = async () => {
-    const response = await fetch(`${baseURL}/outages/${province}`);
-    const data = await response.json();
-    // loop through data and find the stage
-    const stages = [];
-    console.log(data);
-    data.forEach((detail) => {
-      if (!stages.includes(detail.stage)) {
-        stages.push(detail.stage);
-      }
-    });
-    return stages;
-  };
-
   const handleClick = async () => {
-    const details = await fetchDetails();
-    const stages = await findStages();
-    const days = nextThreeDays();
-    const outages = [];
-
-    days.forEach((day) => {
-      stages.forEach((stage) => {
-        details.outages.forEach((outage) => {
-          if (outage.stage === stage && day.getDate() === outage.day1_of_recurrence) {
-            outages.push(outage);
-          }
-        });
-      });
-    });
-
-    days.forEach((day) => {
-      console.log(day.getDate());
-      outages.forEach((outage) => {
-        if (day.getDate() === outage.day1_of_recurrence) {
-          console.log(outage);
-        }
-      });
-      console.log('----------------');
-    });
+    navigate(`/${province}`);
   };
 
   return (
